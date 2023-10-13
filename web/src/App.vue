44241b0f05ab0f6f4e5f27eb841558c9a64250fc
L1nwz1 <a2621481498@outlook.com>
2023-10-13 20:39:34 +0800

    注册和登录

--- web/src/App.vue
+++ web/src/App.vue
@@ -7,7 +7,7 @@
 import NavBar from './components/NavBar.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
-import $ from 'jquery'
+// import $ from 'jquery'
 
 
 export default {
@@ -15,20 +15,26 @@ export default {
     NavBar
   },
   setup() {
-    $.ajax({
-      url: "http://127.0.0.1:3000/user/account/token/",
-      type: "post",
-      data: {
-        username: 'bocchi',
-        password: "test1",
-      },
-      success(resp) {
-        console.log(resp);
-      },
-      error(resp) {
-        console.log(resp);
-      }
-    })
+
+
+
+
+    // $.ajax({
+    //   url: "http://127.0.0.1:3000/user/account/register/",
+    //   type: "post",
+    //   data: {
+    //     username: 'kisama',
+    //     password: "123",
+    //     confirmedPassword: "123",
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // })
+
   }
 }
 </script>
